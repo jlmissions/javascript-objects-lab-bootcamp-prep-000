b2aaa45ec9fdb762cc31a1dd93413ad360e83b24
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-12 01:29:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  delete object.key
+  delete object[key]
   return recipes
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key]
   return recipes
 }
-  
